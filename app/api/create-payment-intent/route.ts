a9375b35c14fdc514dee0038ff2e489482e5e019
Dhairya Tiwari <dhairyatiwari186@gmail.com>
2025-02-15 02:01:47 +0530

    updated create-payment

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+const stripe = new Stripe('sk_test_51QsUghPr6AEsQaoxWITkJ1mJKd7KzsAtBORu80dzKj5chI0M72JgqDn2nmX22eolETnttyrs6wnAm0kiQ5ozJGue00tZjkXKlb');
 
 export async function POST(req: Request) {
   try {
